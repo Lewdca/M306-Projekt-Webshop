d6e50bcc002d716abb29b01256285a8573900fcc
Luca Rickenbach <Luca_Rickenbach@sluz.ch>
2022-05-11 10:05:44 +0200

    Project setup

---
+++ src/test/java/ch/bbzw/m306/Webshop/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbzw.m306.Webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
